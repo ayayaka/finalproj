35626b650d290d595b3958dfe4b32ce486ad29e1
ayayaka <94544383+ayayaka@users.noreply.github.com>
2023-05-21 21:58:02 +0800

    Add files via upload

--- finalproj/index.php
+++ finalproj/index.php
@@ -1,4 +1,8 @@
 <!doctype html>
+
+<?php
+    include "config.php";
+?>
 <html lang="en">
   <head>
     <title>Trippy Vault</title>
@@ -18,6 +22,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+      <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/products.css">
   </head>
   <body>
     
@@ -37,7 +44,7 @@
       </div>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li class="nav-item"><a href="#sectionB" class="nav-link px-2 text-info">New & Featured</a></li>
+        <li class="nav-item"><a href="#sectionB" class="nav-link px-2 text-info">New Products</a></li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-info" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Products</a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -50,76 +57,11 @@
       </ul>
 
       <div class="col-md-2 text-right">
-        <button type="button" class="btn btn-info"><span class="bi bi-cart"></span></button>
-        <a href="" class="btn btn-outline-info me-2" style="text-decoration: none" data-toggle="modal" data-target="#modalLoginForm">Login</a>
-        <a href="" class="btn btn-info" style="text-decoration: none" data-toggle="modal" data-target="#modalRegisterForm">Sign-up</a>
+        <!-- <button type="button" class="btn btn-info"><span class="bi bi-cart"></span></button> -->
+        <a href="login.php" class="btn btn-outline-info me-2" style="text-decoration: none">Login</a>
+        <a href="register.php" class="btn btn-info" style="text-decoration: none">Sign-up</a>
       </div>
     </header>
-</div>
-
-<!-- log in -->
-<div class="container">
-  <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Log In</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-        <div class="md-form mb-5">
-          <label data-error="wrong" data-success="right" for="defaultForm-email">Your username:</label>
-          <input type="email" id="defaultForm-email" class="form-control validate">
-        </div>
-
-        <div class="md-form mb-4">
-          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password:</label>
-          <input type="password" id="defaultForm-pass" class="form-control validate"> 
-        </div>
-
-      </div>
-          <div class="modal-footer d-flex justify-content-center">
-            <button button class="btn btn-info">Log In</button>
-          </div>
-        </div>
-      </div>
-    </div>
-	</div>
-
-  <!-- sign up -->
-<div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-        <div class="md-form mb-5">
-          <label data-error="wrong" data-success="right" for="orangeForm-name">Enter username: </label>
-          <input type="text" id="orangeForm-name" class="form-control validate">
-        </div>
-        <div class="md-form mb-5">
-          <label data-error="wrong" data-success="right" for="orangeForm-email">Enter email: </label>
-          <input type="email" id="orangeForm-email" class="form-control validate">
-        </div>
-
-        <div class="md-form mb-4">
-          <label data-error="wrong" data-success="right" for="orangeForm-pass">Enter password: </label>
-          <input type="password" id="orangeForm-pass" class="form-control validate">
-        </div>
-
-      </div>
-        <div class="modal-footer d-flex justify-content-center">
-          <button class="btn btn-info">Sign up</button>
-        </div>
-    </div>
-  </div>
 </div>
 
     <div class="container-fluid">
@@ -164,124 +106,50 @@
         </div>
     </div>
 
-    <section id="sectionB">
+
     <div class="b-example-divider"></div>
     
     <div class="container-fluid mb-4 mx-4">
-        <h2>NEW & FEATURED PRODUCTS</h2>
+        <h2>FEATURED PRODUCTS</h2>
     </div>
-   
-    <div class="container-fluid w-100">
-    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
-        <div class="carousel-inner row w-10 mx-auto" role="listbox">
-            <div class="carousel-item col-md-4 active text-center">
-                <img class="img-fluid mx-2 d-bloc" src="img/products/1.jpg" alt="slide 1">
-                <p >Gucci Blade Print Beige Tee Large</p>
-                <p >₱1999</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/2.jpg" alt="slide 2">
-                <p >Gucci Interlocking G Stripes TeeLarge</p>
-                <p>₱1999</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/3.jpg" alt="slide 3">
-                <p>Kanye West x AWGE Jik Cross Navy Tee Large</p>
-                <p>₱1499</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/4.jpg" alt="slide 4">
-                <p>Stussy x Nike 8Ball White Tee Large</p>
-                <p>₱1499</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/5.jpg" alt="slide 5">
-                <p>Bape Camou Tote Bag</p>
-                <p>₱599</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/6.jpg" alt="slide 6">
-                <p>Stussy Script Bag Tote Bag</p>
-                <p>₱799</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-auto d-block" src="img/products/7.jpg" alt="slide 7">
-                <p>Carhartt Script Black Sling Bag</p>
-                <p>₱999</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-            <div class="carousel-item col-md-4 text-center">
-                <img class="img-fluid mx-2 d-block" src="img/products/8.jpg" alt="slide 7">
-                <p>Supreme Mesh Mini Red Sling Bag</p>
-                <p>₱999</p>
-                <button type="button" class="btn btn-success"><span class="bi bi-cart"></span> Add to Cart</button>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-            <i class="fa fa-chevron-left fa-lg text-dark"></i>
-        </a>
-        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
-            <i class="fa fa-chevron-right fa-lg text-dark"></i>
-        </a>
+
+    <section class="products">
+      <div class="box-container">
+        <?php  
+            $select_products = mysqli_query($conn, "SELECT * FROM `products` LIMIT 6") or die('query failed');
+            if(mysqli_num_rows($select_products) > 0){
+              while($fetch_products = mysqli_fetch_assoc($select_products)){
+        ?>
+    
+        <form action="" method="post" class="box">
+        <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+        <div class="name"><?php echo $fetch_products['name']; ?></div>
+        <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+        <input type="number" min="1" name="product_quantity" value="1" class="qty">
+        <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+        <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+        <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+        <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+        </form>
+        <?php
+            }
+        }else{
+            echo '<p class="empty">no products added yet!</p>';
+        }
+        ?>
+      </div>
+      <div class="load-more" style="margin-top: 2rem; text-align:center">
+        <a href="shop.php" class="option-btn">load more</a>
+      </div>
+  </section>
+
     </div>
   </div>
 </div>
-</section>
-<div class="b-example-divider"></div>
 
-<footer class="text-center text-white" style="background-color: #f1f1f1;">
-  <!-- Grid container -->
-  <div class="container-fluid pt-4" style="width=100%">
-    <!-- Section: Social media -->
-    <section class="mb-4">
-    <!-- FACEBOOK -->
-      <a
-        class="btn btn-link btn-floating btn-lg text-info m-1"
-        href="https://www.facebook.com/paolo.porte.50"
-        role="button"
-        data-mdb-ripple-color="dark"
-        style="font-size: 30px"
-        ><span class="bi bi-facebook"></span></a>
-
-
-      <!-- INSTAGRAM -->
-      <a
-        class="btn btn-link btn-floating btn-lg text-info m-1"
-        href="https://www.instagram.com/_trippyvault/"
-        role="button"
-        data-mdb-ripple-color="dark"
-        style="font-size: 30px"
-        ><span class="bi bi-instagram"></span></a>
-
-        <!-- PHONE -->
-        <a
-        class="btn btn-link btn-floating btn-lg text-info m-1"
-        href="#!"
-        role="button"
-        data-mdb-ripple-color="dark"
-        style="font-size: 30px"
-        ><span class="bi bi-telephone"></span></a>
-        
-    </section>
-    <!-- Section: Social media -->
-  </div>
-  <!-- Grid container -->
-
-  <!-- Copyright -->
-  
-  <div class="container-fluid text-right text-info p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-  <span class="text-right mx-2" style="font-size: 18px"><span class="bi bi-geo-alt"></span> Pagbilao, Quezon</span>
-  <span class="text-right mx-2"  style="font-size: 18px">© 2023 Copyright: Trippy Vault</span> 
-  </div>
-  <!-- Copyright -->
-</footer>
-
+<?php
+    include "footer.php";
+?>
 
   </body>
 </html>
